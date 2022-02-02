8793cc08ef2820dc88e0427808ad47c57fe646e3
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-02-02 10:47:04 -0300

    atividades de fixaçao

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/02-tipos-e-operacoes.js
@@ -0,0 +1,46 @@
+/*Utilize o operador typeof para
+imprimir qual o tipo das variáveis
+ patientId , isEnrolled , patientInfo e patientEmail */
+
+
+/*let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+
+ console.log(typeof patientId)
+ console.log(typeof isEnrolled)
+ console.log(typeof patientInfo)
+ console.log(typeof patientEmail)*/
+
+
+
+
+
+ /*-------------------------- */
+
+ /*Vamos fazer algumas operações simples para encontrarmos
+ a área e o perímetro de um retângulo de base 5 e altura 8.*/
+
+ /*Crie uma costante chamada base e uma variável 
+ chamada height e atribua os seus respectivos valores: 5 e 8.*/
+
+ const base = 5
+ var height = 8
+
+ /*Crie uma constante chamada area e atribua a ela o resultado
+ da multiplicação da base pela altura. Dica: lembre-se de usar sempre
+ o console.log() para imprimir as variáveis e checar os resultados das operações!*/
+
+ const area = base * height
+ console.log("A área é: " + area)
+
+ /*Crie uma constante chamada perimeter e atribua a ela
+  a soma de todos os lados do retângulo.*/
+
+  const perimeter = 5*2 + 8*2
+  console.log("O perímetro é: " + perimeter)
