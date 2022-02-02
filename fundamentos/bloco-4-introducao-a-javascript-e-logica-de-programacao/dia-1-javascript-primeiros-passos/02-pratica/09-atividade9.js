0a63c246199cd01a42cd3a4daf9d9e9ac7f01183
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-02-02 15:43:39 -0300

    atividade 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/02-pratica/09-atividade9.js
@@ -0,0 +1,13 @@
+/* Escreva um programa que defina três números em constantes e retorne true
+se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bonus: use somente um if . */
+
+const1 = 10
+const2 = 22
+const3 = 30
+
+if(const1%2 !==0 || const2%2 !==0 || const3%2 !==0) {
+    console.log(true)
+}else {
+    console.log(false)
+}
