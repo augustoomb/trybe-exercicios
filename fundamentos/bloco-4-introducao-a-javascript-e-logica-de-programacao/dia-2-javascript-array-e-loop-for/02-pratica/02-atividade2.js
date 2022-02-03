ed0fd617f9722b2532e8790df25abe5c031f04da
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-02-03 08:50:18 -0300

    exercicio 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/02-pratica/02-atividade2.js
@@ -0,0 +1,10 @@
+/* Some todos os valores contidos no array e imprima o resultado */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+
+for(let number of numbers) {
+    total = total + number
+}
+
+console.log(total)
