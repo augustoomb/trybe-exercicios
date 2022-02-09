6a04a857e3b5c879fae95e9744a3586d03688b03
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-02-09 08:35:42 -0300

    pequena correcao de arquivo do dia 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/02-pratica/05-atividade5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/02-pratica/05-atividade5.js
@@ -1,6 +1,6 @@
 /* Utilizando for , descubra qual o maior valor contido no array e imprima-o */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorValor = 0;
 
 for(let i=0; i<numbers.length; i+=1){
@@ -9,4 +9,14 @@ for(let i=0; i<numbers.length; i+=1){
     }
 }
 
-console.log(maiorValor)
+console.log(maiorValor)*/
+
+const n = 9
+
+let resultado = 100
+
+for (let i = 0; i <= n; i+=1) {
+    resultado -= i;    
+}
+
+console.log(resultado);
