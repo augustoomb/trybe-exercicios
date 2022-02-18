703e64e0335cd8ffa052ea312fa8986fb44aa0ce
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-02-18 15:12:48 -0300

    exercicios de fixaçao completos

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/01-fixacao/parteIV-script.js
@@ -0,0 +1,30 @@
+// SELETORES
+const HREF_LINK = document.querySelector("#href");
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+
+
+/*1 - Adicione um evento click no elemento a que possui o id href. Na função do evento, implemente o método .preventDefault() para previnir
+o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer. */
+HREF_LINK.addEventListener("click", function(evento){
+    evento.preventDefault()
+})
+
+
+/*2 - Adicione um evento click no elemento checkbox que possui o id input-checkbox . 
+Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. 
+Ou seja, ao clicar na caixinha, nada deve acontecer.*/
+INPUT_CHECKBOX.addEventListener("click", function(evento){
+    evento.preventDefault()
+})
+
+
+/*3 - Adicione um evento keypress no elemento input type="text" que possui o id input-text . 
+Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra 'a' minúscula) 
+possa ser digitado na caixinha. ( dica: para capturar a tecla digitada, utilize o event.key )*/
+INPUT_TEXT.addEventListener("keypress",function(evento){
+    //console.log(evento.key)
+    if(evento.key != 'a'){
+        evento.preventDefault()
+    }
+})
