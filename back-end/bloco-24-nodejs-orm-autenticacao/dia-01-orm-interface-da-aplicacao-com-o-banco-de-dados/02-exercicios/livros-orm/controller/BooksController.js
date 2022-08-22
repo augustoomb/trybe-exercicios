a114ce479010fce1b730bf9dbce904e2001e785c
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-08-22 11:02:12 -0300

    restante das atividades do dia 24.1

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/02-exercicios/livros-orm/controller/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/02-exercicios/livros-orm/controller/BooksController.js
@@ -20,10 +20,36 @@ router.get('/', async (_req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { title, author, pageQuantity } = req.body;
-  const newBook = await bookServices.create(title, author, pageQuantity);
+  const bookObj = req.body;
+
+  const newBook = await bookServices.create(bookObj);
 
   res.status(201).json(newBook);
+});
+
+router.put('/:id', async (req, res) => {
+  const bookObj = req.body;
+  const { id } = req.params;
+
+  const updatedBook = await bookServices.update(id, bookObj);
+
+  if (updatedBook[0]) {
+    return res.status(200).json({ message: 'book updated!' });
+  }
+
+  return res.status(404).json({ message: 'Book not found!' });
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const deletedBook = await bookServices.remove(id);
+
+  if (deletedBook) {
+    return res.status(200).json({ message: 'book deleted!' });
+  }
+
+  return res.status(404).json({ message: 'Book not found!' });
 });
 
 module.exports = router;
--- back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/02-exercicios/livros-orm/services/booksServices.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/02-exercicios/livros-orm/services/booksServices.js
@@ -12,10 +12,33 @@ const getById = async (id) => {
   return book;
 };
 
-const create = async (title, author, pageQuantity) => {
-  const newBook = await Book.create({ title, author, pageQuantity });
+const create = async (bookObj) => {
+  // const { title, author, pageQuantity } = bookObj;
+  // const newBook = await Book.create({ title, author, pageQuantity });
+  const newBook = await Book.create(bookObj);
 
   return newBook;
 };
 
-module.exports = { getAll, getById, create };
+const update = async (bookId, bookObj) => {
+  const updatedBook = await Book.update(
+    bookObj,
+    { where: { id: bookId } },
+  );
+
+  // console.log(updatedBook); // [1] ou [0]
+
+  return updatedBook;
+};
+
+const remove = async (bookId) => {
+  const deletedBook = await Book.destroy(
+    { where: { id: bookId } },
+  );
+
+  // console.log(deletedBook); // 1 ou 0
+
+  return deletedBook;
+};
+
+module.exports = { getAll, getById, create, update, remove };
